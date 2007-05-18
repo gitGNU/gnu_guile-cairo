d6c9525b907b79b5c3face81d472e0f75c6e37b6
Andy Wingo <wingo@pobox.com>
2007-05-18 13:39:11 +0200

    2007-05-18  Andy Wingo  <wingo@pobox.com>
    	* AUTHORS: I'm the author :)
    
    	* COPYING: 
    	* guile-cairo/guile-cairo.h: 
    	* guile-cairo/guile-cairo.c: 
    	* guile-cairo/guile-cairo-vector-types.h: 
    	* guile-cairo/guile-cairo-vector-types.c: 
    	* guile-cairo/guile-cairo-smob-types.h: 
    	* guile-cairo/guile-cairo-smob-types.c: 
    	* guile-cairo/guile-cairo-enum-types.h: 
    	* guile-cairo/guile-cairo-enum-types.c:
    	* cairo.scm: 
    	* cairo/vector-types.scm: 
    	* cairo/config.scm.in: Fix up license: it is LGPL 2.1 or higher.

--- guile-cairo/guile-cairo-smob-types.h
+++ guile-cairo/guile-cairo-smob-types.h
@@ -3,22 +3,22 @@
  *
  * guile-cairo-smob-types.h:
  *
- * This program is free software; you can redistribute it and/or    
- * modify it under the terms of the GNU General Public License as   
- * published by the Free Software Foundation; either version 2 of   
- * the License, or (at your option) any later version.              
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
  *                                                                  
- * This program is distributed in the hope that it will be useful,  
- * but WITHOUT ANY WARRANTY; without even the implied warranty of   
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    
- * GNU General Public License for more details.                     
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
  *                                                                  
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, contact:
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, contact:
  *
- * Free Software Foundation           Voice:  +1-617-542-5942
- * 59 Temple Place - Suite 330        Fax:    +1-617-542-2652
- * Boston, MA  02111-1307,  USA       gnu@gnu.org
+ * Free Software Foundation, Inc.     Voice:  +1-617-542-5942
+ * 51 Franklin St, Fifth Floor        Fax:    +1-617-542-2652
+ * Boston, MA  02110-1301,  USA       gnu@gnu.org
  */
 
 #ifndef __GUILE_CAIRO_SMOB_TYPES_H__
