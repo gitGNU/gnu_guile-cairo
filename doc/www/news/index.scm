22812a1b268cc8c77a73b1504c31957336863f34
Andy Wingo <wingo@pobox.com>
2007-06-08 00:17:49 +0200

    2007-06-08  Andy Wingo  <wingo@pobox.com>
    	* doc/release: Update with some details.
    	
    	* doc/www/news/index.scm: Add news entry about release.

--- doc/www/news/index.scm
+++ doc/www/news/index.scm
@@ -5,6 +5,11 @@
      (@ (date "18 May 2007")
         (title "initial release"))
      (code "guile-cairo") " 1.3.90 released.")
+
+    (news
+     (@ (date "8 June 2007")
+        (title "guile-cairo 1.3.91 available"))
+     (code "guile-cairo") " 1.3.91 has been released, featuring Cairo 1.2 support.")
     ))
 
 
