fe9e79d5eefba85c5981304c0a11f446909967fc
Andy Wingo <wingo@pobox.com>
2007-07-21 23:00:37 +0200

    2007-07-21  Andy Wingo  <wingo@pobox.com>
    	* tests/unit-tests/Makefile.am: Add api-stability.scm check, and a
    	rule to regenerate a .api file.
    
    	* tests/unit-tests/update-api: New file, a script to update a .api
    	file.
    
    	* tests/unit-tests/cairo.api: New file, the set of symbols
    	exported by (cairo).
    
    	* tests/unit-tests/api-stability.scm: New file, a unit test that
    	checks that no symbol that cairo exports is ever removed from its
    	API.

---
+++ tests/unit-tests/api-stability.scm
@@ -0,0 +1,57 @@
+;;; ----------------------------------------------------------------------
+;;;    unit test
+;;;    Copyright (C) 2007 Andy Wingo
+;;;
+;;;    This program is free software; you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License as published by
+;;;    the Free Software Foundation; either version 2 of the License, or
+;;;    (at your option) any later version.
+;;;
+;;;    This program is distributed in the hope that it will be useful,
+;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;    GNU General Public License for more details.
+;;;
+;;;    You should have received a copy of the GNU General Public License
+;;;    along with this program; if not, write to the Free Software
+;;;    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+;;; ----------------------------------------------------------------------
+
+(use-modules (unit-test)
+             (oop goops)
+             (cairo)
+             ((srfi srfi-1) #:select (lset-difference)))
+
+(define (module-all-uses module)
+  (let ((direct (module-uses module)))
+    (append direct
+            (apply append (map module-all-uses direct)))))
+
+(define (module-exports module)
+  (module-map (lambda (k v) k) module))
+
+(define (module-all-exports module-name)
+  (let ((interface (resolve-interface module-name)))
+    (map
+     string->symbol
+     (sort
+      (apply
+       append
+       (map
+        (lambda (module)
+          (map symbol->string (module-exports module)))
+        (cons interface (module-all-uses interface))))
+      string<?))))
+
+(define-class <test-api> (<test-case>))
+
+(define-method (test-api (self <test-api>))
+  (let ((expected (call-with-input-file "cairo.api" read))
+        (actual (module-all-exports '(cairo))))
+    (let ((new (lset-difference eq? actual expected)))
+      (if (not (null? new))
+          (warning "New API, run make cairo.api.update" new)))
+    (assert-equal '()
+                  (lset-difference eq? expected actual))))
+
+(exit-with-summary (run-all-defined-test-cases))
