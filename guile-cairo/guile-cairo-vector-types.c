b9c556486910b2b58afb039ab64cc880a5704cc7
Andy Wingo <wingo@pobox.com>
2011-04-22 12:00:52 +0200

    upgrade to lgpl v3+
    * COPYING:
    * COPYING.LESSER: Upgrade to the LGPL version 3 or later.
    
    * Makefile.am:
    * cairo.scm:
    * cairo/Makefile.am:
    * cairo/config.scm.in:
    * cairo/vector-types.scm:
    * configure.ac:
    * doc/generate-docs.mk:
    * env.in:
    * examples/Makefile.am:
    * guile-cairo/Makefile.am:
    * guile-cairo/guile-cairo-enum-types.c:
    * guile-cairo/guile-cairo-enum-types.h:
    * guile-cairo/guile-cairo-smob-types.c:
    * guile-cairo/guile-cairo-smob-types.h:
    * guile-cairo/guile-cairo-vector-types.c:
    * guile-cairo/guile-cairo-vector-types.h:
    * guile-cairo/guile-cairo.c:
    * guile-cairo/guile-cairo.h:
    * tests/Makefile.am:
    * tests/unit-tests/Makefile.am:
    * tests/unit-tests/api-stability.scm:
    * tests/unit-tests/update-api:
    * tests/unit-tests/version.scm: Update copyright headers.

--- guile-cairo/guile-cairo-vector-types.c
+++ guile-cairo/guile-cairo-vector-types.c
@@ -1,11 +1,11 @@
 /* guile-cairo
- * Copyright (C) 2007 Andy Wingo <wingo at pobox dot com>
+ * Copyright (C) 2007, 2011 Andy Wingo <wingo at pobox dot com>
  *
  * guile-cairo-vector-types.c: Cairo for Guile
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of the
+ * published by the Free Software Foundation; either version 3 of the
  * License, or (at your option) any later version.
  *                                                                  
  * This program is distributed in the hope that it will be useful, but
@@ -14,11 +14,8 @@
  * Lesser General Public License for more details.
  *                                                                  
  * You should have received a copy of the GNU Library General Public
- * License along with this program; if not, contact:
- *
- * Free Software Foundation, Inc.     Voice:  +1-617-542-5942
- * 51 Franklin St, Fifth Floor        Fax:    +1-617-542-2652
- * Boston, MA  02110-1301,  USA       gnu@gnu.org
+ * License along with this program; if not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 #include <libguile.h>
