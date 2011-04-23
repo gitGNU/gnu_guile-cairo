ee42029625a754212fb95073bfa287871daf7653
Andy Wingo <wingo@pobox.com>
2011-04-23 12:35:17 +0200

    bump manual version, add web site news entry
    * doc/guile-cairo.texi: Bump version.
    * doc/www/news/index.scm: Add releases.

--- doc/www/news/index.scm
+++ doc/www/news/index.scm
@@ -2,6 +2,27 @@
   '((h2 "news")
 
     (news
+     (@ (date "23 April 2011")
+        (title "Guile-Cairo 1.9.90 available"))
+     "Guile-Cairo 1.9.90 has been released, adding support for Cairo API
+from versions 1.6, 1.8, and 1.10.  This is a prerelease.  Give it a try!")
+
+    (news
+     (@ (date "23 April 2011")
+        (title "Guile-Cairo 1.4.1 available"))
+     "Guile-Cairo 1.4.1 has been released, updating to the LGPLv3+.")
+
+    (news
+     (@ (date "28 July 2007")
+        (title "Guile-Cairo 1.4.0 available"))
+     "Guile-Cairo 1.4.0 has been released. We are now API stable! Also
+check the new code, bug list, and new bug links on all web site pages:
+thanks to freedesktop.org for the bugzilla, and launchpad.net for its
+sweet code
+browser. " (a (@ (href "https://code.launchpad.net/~andywingo/guile-cairo/trunk/+subscribe")) "Sign
+up for commit mails!"))
+
+    (news
      (@ (date "28 July 2007")
         (title "Guile-Cairo 1.4.0 available"))
      "Guile-Cairo 1.4.0 has been released. We are now API stable! Also
