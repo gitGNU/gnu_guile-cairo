497057e31d446b95634a7249f5edfec55a1417a2
Andy Wingo <wingo@pobox.com>
2011-11-22 21:09:53 +0100

    update remaining gna references
    * README:
    * doc/www/dev/index.scm:
    * doc/www/download/index.scm: Fix the remaining gna references.

--- doc/www/dev/index.scm
+++ doc/www/dev/index.scm
@@ -1,23 +1,22 @@
-(define main-archive-url "http://arch.gna.org/guile-cairo/trunk")
-
 (define page
   `((h2 "developer information")
 
     (h3 "dependencies")
 
-    (p (code "guile-cairo") " currently requires Cairo 1.2,
-although patches providing back compatibility will be accepted.")
+    (p (code "guile-cairo") " wraps the API from Cairo version 1.10 and
+later, though you can build it with Cairo as old as 1.2.")
 
     (p (code "guile-cairo") " works with the current "
-       (a (@ (href "http://www.gnu.org/software/guile/")) "Guile") " 1.8.")
+       (a (@ (href "http://www.gnu.org/software/guile/")) "Guile") "
+version 2.0, as well as with the previous 1.8 stable series.")
 
     (h3 "source repository")
 
     (p "guile-cairo is managed with "
-       (a (@ (href "http://bazaar-vcs.org/")) "bzr") ", a distributed "
+       (a (@ (href "http://git-scm.org/")) "git") ", a distributed "
        "version control system. To grab guile-cairo, run the following:")
 
-    (pre "bzr get " ,main-archive-url " guile-cairo\n"
+    (pre "git clone git://git.savannah.nongnu.org/guile-cairo.git\n"
          "cd guile-cairo\n"
          "./autogen.sh --prefix=... && make")
 
@@ -25,13 +24,11 @@ although patches providing back compatibility will be accepted.")
        (code "make install") ", or run it uninstalled using the "
        (code "env") " script.")
 
-    (h3 "browse the code, get commit mails, etc")
+    (h3 "browse the code")
 
-    (p "Our source code, while hosted primarily at gna.org, is mirrored
-over at " (a (@ (href "http://code.launchpad.net/guile-cairo/")) "launchpad.net") ".
-Visit that page to browse the source code, register your
-branch, " (a (@ (href "https://code.launchpad.net/~andywingo/guile-cairo/trunk/+subscribe")) "sign
-up for commit mails") ", etc.")
+    (p "Browse the source code on "
+       (a (@ (href "http://git.savannah.nongnu.org/cgit/guile-cairo.git/"))
+          "Savannah's cgit instance") ".")
 
     (h3 "patches and bug reports")
 
@@ -39,13 +36,13 @@ up for commit mails") ", etc.")
 our " (a (@ (href "https://bugs.freedesktop.org/enter_bug.cgi?product=guile-cairo")) "bug
 tracker") " hosted by freedesktop.org. If you're more of the mailing
 list type, you can send queries to " (code "guile-user") ", and copy Andy
-Wingo (" (code "wingo pobox com") ").")
+Wingo (" (code "wingo@pobox.com") ").")
 
     (h3 "gna project page")
 
     (p "We also have
-a " (a (@ (href "http://gna.org/projects/guile-cairo")) "page
-on GNA!") ".")))
+a " (a (@ (href "https://savannah.nongnu.org/projects/guile-cairo")) "page
+on savannah") ".")))
 
 (load "../template.scm")
 (define (make-index)
--- doc/www/download/index.scm
+++ doc/www/download/index.scm
@@ -1,10 +1,14 @@
 (define page
   '((h2 "releases")
 
-    (p "Releases are available from our "
-       (a (@ (href "http://download.gna.org/guile-cairo/"))
+    (p "Current releases are available from our "
+       (a (@ (href "http://download.savannah.gnu.org/releases/guile-cairo/"))
           "release area")
-       " on gna.org.")))
+       " on savannah.")
+    (p "Before moving to Savannah, guile-cairo was on GNA!, so you can find the older releases "
+       (a (@ (href "http://download.gna.org/guile-cairo/"))
+          "over there")
+       ".")))
 
 (load "../template.scm")
 (define (make-index)
